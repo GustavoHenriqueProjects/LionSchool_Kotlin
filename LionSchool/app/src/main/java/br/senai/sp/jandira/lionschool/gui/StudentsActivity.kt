2b5b983f545469b9833f56cf0e0996372c62e2e1
Gustavo Henrique <Gustavo326projetos@gmail.com>
2023-05-23 18:10:25 -0300

    Iniciando endpoint alunos de ds

--- LionSchool/app/src/main/java/br/senai/sp/jandira/lionschool/gui/StudentsActivity.kt
+++ LionSchool/app/src/main/java/br/senai/sp/jandira/lionschool/gui/StudentsActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -14,14 +17,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.lionschool.R
-import br.senai.sp.jandira.lionschool.gui.ui.theme.LionSchoolTheme
+import br.senai.sp.jandira.lionschool.theme.LionSchoolTheme
 
 class StudentsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -123,6 +127,51 @@ fun InterfaceStudents() {
                                 Text(text = item, style = MaterialTheme.typography.body1)
                             }
                         }
+                    }
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .width(190.dp)
+                    .height(270.dp),
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                    .background(color = Color.Black)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.background_students),
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.FillBounds
+                        )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceAround
+                    ) {
+                        // Conteúdo adicional do card
+                        Image(
+                            painter = painterResource(id = R.drawable.student),
+                            contentDescription = "")
+
+                        Text(
+                            text = "Título",
+                            modifier = Modifier.padding(16.dp),
+                            style = TextStyle(
+                                color = Color.White,
+                                fontSize = 24.sp,
+                            fontWeight = FontWeight.Bold)
+                        )
                     }
                 }
             }
